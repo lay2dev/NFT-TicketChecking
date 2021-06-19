04129ea185ea52f2760fd89a39faacb20a96ef21
eclipes <751733381@qq.com>
2021-06-19 09:33:32 +0800

    fix(check):dialog close  modal

--- pages/check/index.vue
+++ pages/check/index.vue
@@ -8,22 +8,16 @@
           <template v-if="status === 'success'">
             <img class="icon" src="~/assets/img/success.svg" />
             <div class="status">#{{ ticketId }}，验票成功!</div>
-            <div class="tip">
-              验票时间：{{ dayjs().format('YYYY年M月D日 HH:mm') }}
-            </div>
+            <div class="tip">验票时间：{{ dayjs().format('YYYY年M月D日 HH:mm') }}</div>
           </template>
           <template v-else-if="status === 'fail'">
             <img class="icon" src="~/assets/img/fail.svg" />
             <div class="status">验票失败!</div>
-            <div class="tip">
-              验票时间：{{ dayjs().format('YYYY年M月D日 HH:mm') }}
-            </div>
+            <div class="tip">验票时间：{{ dayjs().format('YYYY年M月D日 HH:mm') }}</div>
           </template>
           <template v-else>
             <div class="status">待验票中</div>
-            <el-button round type="primary" :loading="loading"
-              >验票中。。</el-button
-            >
+            <el-button round type="primary" :loading="loading">验票中。。</el-button>
           </template>
         </div>
       </div>
@@ -109,6 +103,7 @@ export default {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         inputValue: this.targetArgs,
+        closeOnClickModal: false,
       })
         .then(({ value }) => {
           this.targetArgs = value
