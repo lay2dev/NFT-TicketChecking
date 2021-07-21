2f8e06091771b470e7afcdac234ab1eb97e651f9
johnz <johnz@lay2.dev>
2021-07-21 20:47:07 +0800

    update .env.example

--- bin/publish-cdn.sh
+++ bin/publish-cdn.sh
@@ -8,25 +8,25 @@ then
   ln -s .env.${NODE_ENV} .env
 fi
 
-TARGET="$SSH_USER@$SSH_DOMAIN"
-echo "target is $TARGET"
+# TARGET="$SSH_USER@$SSH_DOMAIN"
+# echo "target is $TARGET"
 
 
 yarn build
 # 打包
-tar -czf dist.tgz dist
-# 删除
-ssh -t -p 22 $TARGET "cd /var/www/$SSH_DOMAIN && rm -rf ../$SSH_DOMAIN/*"
-# 上传到目录
-scp dist.tgz $TARGET:/var/www/$SSH_DOMAIN/
-# 解压
-ssh -t -p 22 $TARGET "cd /var/www/$SSH_DOMAIN && tar -zxf dist.tgz; rm dist.tgz"
-# 覆盖
-ssh -t -p 22 $TARGET "cd /var/www/$SSH_DOMAIN &&mv dist/* ../$SSH_DOMAIN/"
-# 删除
-rm dist.tgz
+# tar -czf dist.tgz dist
+# # 删除
+# ssh -t -p 22 $TARGET "cd /var/www/$SSH_DOMAIN && rm -rf ../$SSH_DOMAIN/*"
+# # 上传到目录
+# scp dist.tgz $TARGET:/var/www/$SSH_DOMAIN/
+# # 解压
+# ssh -t -p 22 $TARGET "cd /var/www/$SSH_DOMAIN && tar -zxf dist.tgz; rm dist.tgz"
+# # 覆盖
+# ssh -t -p 22 $TARGET "cd /var/www/$SSH_DOMAIN &&mv dist/* ../$SSH_DOMAIN/"
+# # 删除
+# rm dist.tgz
 # 上传cdn
-# node bin/publish-cdn.js
+node bin/publish-cdn.js
 
 # 备注：上传公钥命令
 #  ssh-copy-id -i ~/.ssh/id_rsa.pub user@domain.com
